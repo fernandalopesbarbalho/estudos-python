27d53e06b4f9040c30a8901c7bb232524653e8ef
Fernanda Barbalho <137642560+fernandalopesbarbalho@users.noreply.github.com>
2024-05-23 11:39:06 -0300

    Create manipulando-numeros.py

---
+++ manipulando-numeros.py
@@ -0,0 +1,13 @@
+#maior e menor número
+maiorNum = max(1, 2, 3)
+menorNum = min(1, 2, 3)
+print("maior número:", maiorNum)
+print("menor número:", menorNum)
+
+#imprimir números decimais
+num = 3.1415926
+print('%.2f'%num)
+
+#formatar números
+num = 100
+print("preço: {} reais".format(num))
